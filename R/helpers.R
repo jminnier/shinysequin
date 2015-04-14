08de2ca5d6f20acd776215677efd36d4a1cbb0a0
jminnier <jessica.minnier@gmail.com>
2015-04-14 16:33:44 -0700

    Worked on dotplot function and related helper functions.

--- R/helpers.R
+++ R/helpers.R
@@ -1,16 +1,34 @@
 #' Retrieve the counts from a data set.
 #'
 #' This function retrieves a data set containing the vectors of counts and
-#' one gene identifier.
+#' selected gene identifiers.
+#' @param groupvec a character vector of group names (i.e. tissue type), assuming
+#' column names of counts include these characters
+#' @param id_vars a character vector of gene identifier column names
+#' @param data a data frame that includes counts and gene identifiers
 #' @examples
-#' mycounts <- get_count_data(groupvec=c("tissueA","tissueB","tissueC"),data=countdata, geneid_ind=1)
+#' mycounts <- get_count_data(groupvec=c("tissueA","tissueB","tissueC"),data=countdata, id_vars)
 #' head(mycounts)
-get_count_data <- function(groupvec, data, geneid_ind=1) {
+get_count_data <- function(groupvec, id_vars, data) {
   ind_group <- lapply(groupvec,function(k) grep(k,colnames(data)))
   ind_group_all <- unlist(ind_group)
   if(length(ind_group_all)==0) stop("Items in groupvec do not match column names")
-  datacounts <- dplyr::bind_cols(data%>%select(geneid_ind),data[,ind_group_all])
-  colnames(datacounts)[1] = "geneid"
+  datacounts <- data[,c(match(id_vars,colnames(data)),ind_group_all)]
   return(datacounts)
 }
 
+#' Creates long format count data.
+#'
+#' @param groupvec a character vector of group names (i.e. tissue type), assuming
+#' column names of counts include these characters
+#' @param id_vars a character vector of gene identifier column names
+#' @param data a data frame that includes counts and gene identifiers
+#' @examples
+#' mycountslong <- get_count_data_long(groupvec=c("tissueA","tissueB","tissueC"),data=countdata, id_vars = c("Geneid","MGIID","Chr"))
+#' head(mycountslong)
+get_count_data_long <- function(groupvec, id_vars, data) {
+  datacounts <- get_count_data(groupvec, id_vars, data)
+  datacountlong <- datacounts%>%reshape2::melt(id.vars=id_vars,value.name="Count",variable.name="Sample")
+  datacountlong <- datacountlong%>%tidyr::separate(Sample,c("Group","SampleNum"))
+  datacountlong
+}
--- R/plotting.R
+++ R/plotting.R
@@ -3,6 +3,40 @@
 #'
 #' This function plots a dotplot.
 #'
-dotplot_ggvis <- function() {
-  print("dot, plot!")
+dotplot_genes <- function(sel_groups, sel_genes, gene_col, data, log2y=TRUE) {
+  datalong <- get_count_data_long(groupvec = sel_groups, id_vars = gene_col, data = data)
+  #Need to adapt to allow for counts OR rpkm data OR cpms
+  datalong$gene <- datalong[,gene_col]
+  subdat <- datalong%>%dplyr::filter(gene%in%sel_genes)
+  if(log2y) {
+    subdat$y = log2(subdat$Count)
+    ylab <- "log2(Count)"
+  }else{
+    subdat$y = subdat$Count
+    ylab = "Count"
+  }
+
+  p <- ggplot(subdat,aes(x=Group,y=y,fill=Group)) +
+    ggplot2::geom_boxplot()+
+    ggplot2::ylab(ylab)+
+    ggplot2::facet_grid(~gene,scales="free_y") +
+    ggplot2::geom_point(size=3)+
+    ggplot2::stat_summary(fun.y=mean, geom="point", shape=5, size=3)+
+    ggplot2::scale_fill_discrete(name="Group",breaks=sort(sel_groups),
+                        labels=sel_groups,
+                        guide=ggplot2::guide_legend(keyheight=4,keywidth=2))+
+    ggplot2::theme_bw(base_family="mono") +
+    ggplot2::theme(
+      plot.title=element_text(face="bold",size = rel(2)),
+      strip.text = element_text(face="bold",size=rel(2)),
+      strip.background=element_rect(fill="lightgrey"),
+      axis.title = element_text(size=rel(2),color="blue"),
+      axis.text = element_text(size=rel(2)),
+      legend.text=element_text(size=rel(2)),
+      legend.title=element_text(size=rel(2)),
+      legend.position="bottom",legend.direction="vertical"
+    )+
+    ggplot2::guides(fill = ggplot2::guide_legend(nrow = 3))
+
+  print(p)
 }
